681ed36b0d76dd92d18ba6950d9a7e4cce63532a
Rails Udemy Course <railsudemycourse@gmail.com>
2018-10-26 23:45:09 +0000

    added interface so visitors can crearte a user an sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root 'pages#home'
+  root 'pages#home'
 
-get 'about', to: 'pages#about'
+  get 'about', to: 'pages#about'
 
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
