99459e2a8c5755a1a2959bf616c5862602801a6d
Rails Udemy Course <railsudemycourse@gmail.com>
2018-10-29 16:27:10 +0000

    Added form for editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,21 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:succes] = "Your account was updated successsfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
