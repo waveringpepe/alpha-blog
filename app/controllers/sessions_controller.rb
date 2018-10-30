7da52a25bf757bac8c352c56c5c3fad021c2c234
Rails Udemy Course <railsudemycourse@gmail.com>
2018-10-30 19:42:09 +0000

    Created a sign in button and also users who are not login cannot see the button log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "You are logged in"
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was something wrong with your login information"
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@
   
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
