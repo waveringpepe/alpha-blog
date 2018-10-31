7a36d0e693aa0c10e67c5f9c00467fb630b41e5d
Rails Udemy Course <railsudemycourse@gmail.com>
2018-10-31 17:15:10 +0000

    Fixed problems with user admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
 before_action :set_user, only: [:edit, :update, :show]
 before_action :require_same_user, only: [:edit, :update, :destroy]
-before_Action :require_admin, only: [destroy]
+before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -56,8 +56,8 @@ before_Action :require_admin, only: [destroy]
     @user = User.find(params[:id])
   end
   
-  def require_same_user and !current_user.admin?
-    if current_user != @user
+  def require_same_user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account"
     redirect_to root_path
     end
