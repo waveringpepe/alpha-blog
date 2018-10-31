2da71e29bc56d40ad97366b9ae45d423e48ce090
Rails Udemy Course <railsudemycourse@gmail.com>
2018-10-31 05:33:49 +0000

    added fields for editing your own profile, also restrictions for urls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update, :show]
-
+  before_action :require_user, except: [:new]
+  before_action :require_same_user, only: [:edit, :update]
+  
+  
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -44,5 +47,12 @@ class UsersController < ApplicationController
   
   def set_user
     @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 end
