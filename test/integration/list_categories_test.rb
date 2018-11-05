47b439d5801c6261cccc93d73574a04e7f26dd32
Rails Udemy Course <railsudemycourse@gmail.com>
2018-11-05 23:36:27 +0000

    added integration test updates and a list of categories in the navBar

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
+
 class ListCategoriesTest < ActionDispatch::IntegrationTest
+  
   def setup
     @category = Category.create(name: "sports")
     @category2 = Category.create(name: "programming")
